9ab622cdf613b766f3a0b9c5b4fa52fd69c4ef24
Sebazt <juan.londono29@udea.edu.co>
2023-04-13 00:33:26 -0500

    :safety_vest: add class Estudiante

--- src/main/java/Estudiante.java
+++ src/main/java/Estudiante.java
@@ -1,2 +1,35 @@
 public class Estudiante {
+    private String nombre;
+    private int edad;
+    private double nota;
+    // debo crear el nodo
+    public Estudiante nodo;
+
+
+    public Estudiante(String nombre, int edad, double nota) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nota = nota;
+    }
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
--- src/main/java/PilaLista.java
+++ src/main/java/PilaLista.java
@@ -1,5 +1,50 @@
-public class PilaLista {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class PilaLista {
+
+    public static void imprimir(String data){
+        System.out.println(data);
+    }
+
+    public static String lea(String txt){
+        String data = "";
+        InputStreamReader flujo = new InputStreamReader(System.in);
+        BufferedReader teclado = new BufferedReader(flujo);
+        try{
+            imprimir(txt);
+            data=teclado.readLine();
+        }catch (IOException error){
+            System.out.println("Se cometio el error "+error);
+        }
+        return data;
+    }
+
+    public static void impLista(Estudiante cabezaData){
+        //  protejo a cabeza en otra variable
+        Estudiante nodoSiguiente = cabezaData;
+        if (cabezaData == null){
+            imprimir("Lista vacia");
+        }else {
+        while(nodoSiguiente != null){
+            imprimir("Nombre: "+nodoSiguiente.getNombre()+" Edad: "+ cabezaData.getEdad()+ " Nota: "+cabezaData.getNota());
+            nodoSiguiente=nodoSiguiente.nodo;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        imprimir("PROGRAMA PILA CON LISTA DINAMICA");
+        Estudiante cabeza = null;
+        impLista(cabeza);
+        Estudiante obj1 = new Estudiante("Sebastian",29,5.0);
+        obj1.nodo = null;
+        cabeza = obj1;
+        Estudiante obj2 = new Estudiante("Fernanda",23,5.0);
+        obj2.nodo = obj1;
+        cabeza = obj2;
+        impLista(cabeza);
     }
 }
+
